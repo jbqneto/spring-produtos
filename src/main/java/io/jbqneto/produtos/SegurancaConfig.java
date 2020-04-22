6526813d8e0d3456f4d99e807f7c524e8ebfd7db
jbqneto <jbqneto@gmail.com>
2020-04-22 20:16:01 -0300

    inicio da criacao do segurancaConfig e mudanca no gitIgnore

---
+++ src/main/java/io/jbqneto/produtos/SegurancaConfig.java
@@ -0,0 +1,12 @@
+package io.jbqneto.produtos;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+public class SegurancaConfig extends WebSecurityConfigurerAdapter {
+
+	protected void configure() {
+		
+	}
+	
+}
