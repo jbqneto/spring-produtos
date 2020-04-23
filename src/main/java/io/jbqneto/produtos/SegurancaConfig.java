8a2a131e897b707d3ce735d77d88824e1e49074a
jbqneto <jbqneto@gmail.com>
2020-04-22 21:26:13 -0300

    dependencias corretas do Spring Security

--- src/main/java/io/jbqneto/produtos/SegurancaConfig.java
+++ src/main/java/io/jbqneto/produtos/SegurancaConfig.java
@@ -1,8 +1,9 @@
 package io.jbqneto.produtos;
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-
+@EnableWebSecurity
 public class SegurancaConfig extends WebSecurityConfigurerAdapter {
 
 	protected void configure() {
