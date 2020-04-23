cd3034faef0e612b560b09464b6abecff0b7f04e
jbqneto <jbqneto@gmail.com>
2020-04-23 00:14:58 -0300

    Adicionado BASIC AUTH com usuario e senha fixos

--- src/main/java/io/jbqneto/produtos/SegurancaConfig.java
+++ src/main/java/io/jbqneto/produtos/SegurancaConfig.java
@@ -7,21 +7,27 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import io.jbqneto.produtos.config.BasicAuthentication;
+
 @EnableWebSecurity
 public class SegurancaConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	BasicAuthentication basicAuth;
 
 	@Autowired
 	protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("jbqneto").password("3101").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("jbqneto").password("{noop}3101").roles("USER");
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
 		http.authorizeRequests()
 			.antMatchers(HttpMethod.GET, "/api/**").permitAll()
 			.antMatchers("/api/**").authenticated()
-			.and()
-			.cors();
+			.and().cors();
+		http.httpBasic().authenticationEntryPoint(basicAuth);
 	}
 	
 }
