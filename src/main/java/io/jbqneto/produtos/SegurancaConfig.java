669e8344e735d699ff41cf355e2a5547568dff4d
jbqneto <jbqneto@gmail.com>
2020-04-22 22:03:29 -0300

    regras de autenticação para permitir apenas o GET

--- src/main/java/io/jbqneto/produtos/SegurancaConfig.java
+++ src/main/java/io/jbqneto/produtos/SegurancaConfig.java
@@ -1,13 +1,20 @@
 package io.jbqneto.produtos;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SegurancaConfig extends WebSecurityConfigurerAdapter {
 
-	protected void configure() {
-		
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, "/api/**").permitAll()
+			.antMatchers("/api/**").authenticated()
+			.and()
+			.cors();
 	}
 	
 }
