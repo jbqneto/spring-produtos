eed3eb462623d1ad2cd91318525ac2bf27561268
jbqneto <jbqneto@gmail.com>
2020-04-22 23:19:28 -0300

    GlobalConfigure do arquivo de segurança

--- src/main/java/io/jbqneto/produtos/SegurancaConfig.java
+++ src/main/java/io/jbqneto/produtos/SegurancaConfig.java
@@ -1,6 +1,8 @@
 package io.jbqneto.produtos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,6 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SegurancaConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("jbqneto").password("3101").roles("ADMIN");
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
